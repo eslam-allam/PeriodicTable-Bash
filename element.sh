1d75d398ab1c3a00b47fc5fd35f107095616958f
eslam-allam <eslamallam73@gmail.com>
2022-07-23 03:06:15 +0000

    feat: Added program structure

---
+++ element.sh
@@ -0,0 +1,35 @@
+# !/bin/bash
+
+# Get passed variable
+
+# If empty
+
+  # Echo appropriate message and exit
+
+# If variable is a number
+
+  # Atomic number = variable
+
+  # Query column = atomic_number
+
+# If variable is a character
+
+  # Symbol = variable
+
+  # Query column = symbol
+
+# If variable is a word
+
+  # Name = variable
+
+  # Query column = name
+
+# Get atomic number, name, symbol, type, mass, melting point, and boiling point using query column
+
+# If doesn't exist
+
+  # Display not found message
+
+# Else display element information
+
+
