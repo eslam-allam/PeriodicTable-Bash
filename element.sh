b73b8dd94251a958c3b5d67a07bdede4d4d13040
eslam-allam <eslamallam73@gmail.com>
2022-07-23 03:32:28 +0000

    feat: Added conditions for checking input type

--- element.sh
+++ element.sh
@@ -1,30 +1,41 @@
 # !/bin/bash
 
 # Get passed variable
+PASSED_VARIABLE=$1
 
 # If empty
+if [[ -z $PASSED_VARIABLE ]] 
+then
 
   # Echo appropriate message and exit
-
+  echo Please provide an element as an argument.
+  exit
+fi
 # If variable is a number
+if [[ $PASSED_VARIABLE =~ ^[0-9]+$ ]]
+then
 
-  # Atomic number = variable
+  # Query condition = atomic_number
+  CONDITION="atomic_number"
 
-  # Query column = atomic_number
+# If variable is a character (regex only works with the elements in this database
+elif [[ $PASSED_VARIABLE =~ ^[Aa-Zz]{1,2}$ ]] 
+then
 
-# If variable is a character
-
-  # Symbol = variable
-
-  # Query column = symbol
+  # Query condition = symbol
+  CONDITION="symbol"
 
 # If variable is a word
+elif [[ $PASSED_VARIABLE =~ ^[Aa-Zz]{3,} ]]
+then
 
-  # Name = variable
+  # Query condition = name
+  CONDITION="name"
+fi
 
-  # Query column = name
+echo $CONDITION
 
-# Get atomic number, name, symbol, type, mass, melting point, and boiling point using query column
+# Get atomic number, name, symbol, type, mass, melting point, and boiling point using query condition
 
 # If doesn't exist
 
